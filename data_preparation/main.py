d6e22c5570d5ee8b281a4397527e715bd2dbfa61
mojmas <masoudi.m1991@gmail.com>
2023-08-08 17:01:00 +0100

    cleaning main

--- data_preparation/main.py
+++ data_preparation/main.py
@@ -6,114 +6,10 @@ import os
 import shutil
 from tqdm import tqdm
 from PIL import Image
+from sampling import sampling_uniform, sampling_stratified, sampling_fixed_number
+from sampling import load_uvp5, load_uvp6, load_uvp6_from_csv, copy_image_from_df
 
 
-def sampling_stratified(df, percent):
-    # shffle data
-    df = df.sample(frac=1, random_state=np.random.seed())
-    grouped = df.groupby('groundtruth')
-
-    # Initialize an empty DataFrame to store the sampled rows
-    sampled = pd.DataFrame()
-
-    # For each group type, select 10 percent of the rows randomly
-    for group_name, group_data in grouped:
-        sample = group_data.sample(frac=percent, random_state=42)
-        sampled = pd.concat([sampled, sample])
-
-    return sampled
-
-
-def sampling_uniform(df, percent):
-    # shffle data
-    df = df.sample(frac=1, random_state=np.random.seed())
-
-    group_sizes = df.groupby('groundtruth').size()
-    group_sample_sizes = (group_sizes / group_sizes.sum() * len(df) * percent).round().astype(int)
-    min_sample_size = group_sample_sizes.min()
-
-    # Sample the same number of data points from each class
-    sampled_df = df.groupby('groundtruth', group_keys=False).apply(
-        lambda x: x.sample(n=min_sample_size, replace=True) if len(x) < min_sample_size else x.sample(n=min_sample_size,
-                                                                                                      replace=False))
-
-    return sampled_df
-
-
-def sampling_fixed_number(df, fixed_number):
-    # shffle data
-    df = df.sample(frac=1, random_state=np.random.seed())
-    sampled_df = df.groupby('groundtruth', group_keys=False).apply(
-        lambda x: x.sample(n=fixed_number, replace=True) if len(x) > fixed_number else x.sample(n=len(x), replace=False))
-
-    return sampled_df
-
-
-def load_uvp5(path):
-    o = pd.read_csv((path + r'\final\objects.tsv.gz'), sep='\t',
-                    usecols=['profile_id', 'object_id', 'group', 'depth', ])
-    s = pd.read_csv((path + r'\final\samples.tsv.gz'), sep='\t', index_col='profile_id',
-                    usecols=['profile_id', 'lat',
-                             'lon', 'datetime',
-                             'pixel_size',
-                             'uvp_model'])
-
-    df = o.join(s, on='profile_id')
-    df = df.rename(columns={'group': 'groundtruth', })
-
-    # dicard ZD values
-    df = df[df['uvp_model'] != 'ZD']
-
-    return df
-
-
-def load_uvp6(path):
-    df = pd.read_csv((path + r'\taxa.csv.gz'), sep=',', usecols=['objid', 'taxon'])
-    df['groundtruth'] = df['taxon']
-    df = df.rename(columns={'objid': 'object_id', })
-
-    df['uvp_model'] = ['UVP6'] * len(df)
-    df['profile_id'] = ['NaN'] * len(df)
-    df['depth'] = ['NaN'] * len(df)
-    df['lat'] = ['NaN'] * len(df)
-    df['lon'] = ['NaN'] * len(df)
-    df['datetime'] = ['NaN'] * len(df)
-    df['pixel_size'] = ['NaN'] * len(df)
-
-    df['taxon'] = df['taxon'].str.replace('<', '_')
-    df['taxon'] = df['taxon'].str.replace(' ', '_')
-
-    df['object_id'] = df['object_id'].astype(int)
-    df['path'] = dir_uvp6 + '\\imgs\\' + df['taxon'].astype(str) + '\\' + df['object_id'].astype(str) + '.png'
-    df['relative_path'] = 'output\\' + df['object_id'].astype(str) + '.png'
-
-    df = df.drop('taxon', axis=1)
-
-    return df
-
-
-def load_uvp6_from_csv(csv_path):
-
-    df = pd.read_csv(csv_path, sep=',').reset_index(drop=True, inplace=False)
-    df = df.rename(columns={'groundthruth': 'groundtruth', })
-
-    df['groundtruth'] = df['groundtruth'].str.replace('fi', 'fiber')
-    df['groundtruth'] = df['groundtruth'].str.replace('_', '<')
-    df['groundtruth'] = df['groundtruth'].str.replace('copepoda<eggs', 'copepoda eggs')
-    df['groundtruth'] = df['groundtruth'].str.replace('Hydrozoa', 'Hydrozoa_others')
-    df['groundtruth'] = df['groundtruth'].str.replace('Mollusca', 'Mollusca_others')
-    df['groundtruth'] = df['groundtruth'].str.replace('fiberber', 'detritus')
-    df['groundtruth'] = df['groundtruth'].str.replace('fiberlament', 'detritus')
-    df['groundtruth'] = df['groundtruth'].str.replace('Creseis acicula', 'Creseis')
-    df['groundtruth'] = df['groundtruth'].str.replace('antenna<Calanoida', 'Calanoida')
-    df = df[df['groundtruth'] != 'Botrynema']
-
-    df['uvp_model'] = ['UVP6'] * len(df)
-    dir1_ = r'D:\mojmas\files\data'
-    df['relative_path'] = df['relative_path'].str.replace(r'\\', '/')
-    df['path'] = dir1_ + r'\\' + df['relative_path'].astype(str)
-
-    return df
 
 
 
@@ -130,12 +26,12 @@ stratified_flag = False
 sampling_percent_uvp5 = 0.0051  # all=9,884,798
 sampling_percent_uvp6 = 0.08  # all=634,459
 
-uvp5_cut_flag = True
+uvp5_cut_flag = False
 
 sample_fixed = True
 if sample_fixed:
-    sampling_percent_uvp5 = 100
-    sampling_percent_uvp6 = 100
+    sampling_percent_uvp5 = 10
+    sampling_percent_uvp6 = 10
 
 target_size = [227, 227]
 
@@ -163,16 +59,6 @@ else:
     df2_sample = sampling_uniform(df2, sampling_percent_uvp6)
 
 
-# create path column uvp5
-df1_sample['profile_id'] = df1_sample['profile_id'].astype(int)
-df1_sample['object_id'] = df1_sample['object_id'].astype(int)
-df1_sample['path'] = dir_uvp5 + '\\images\\' + df1_sample['profile_id'].astype(str) + '\\' + df1_sample['object_id'].astype(str) + '.jpg'
-df1_sample['relative_path'] = 'output\\' + df1_sample['object_id'].astype(str) + '.jpg'
-
-
-# create path column uvp6
-
-
 # create sampling output
 time_str = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
 output_folder = Path(dir_output)
@@ -183,51 +69,8 @@ sampling_path_images = sampling_path / ("output")
 sampling_path_images.mkdir(parents=True, exist_ok=True)
 
 # Loop through the image paths and copy the images to the target directory
-
-# # uvp5
-if uvp5_cut_flag:
-    for index, row in tqdm(df1_sample.iterrows()):
-        image_path = row['path']
-        image_filename = os.path.basename(image_path)
-        target_path = os.path.join(sampling_path_images, image_filename)
-        # crop 31px from bottom
-        img = Image.open(image_path)
-        # crop image
-        width, height = img.size
-        right = width
-        bottom = height - 31
-        cropped_img = img.crop((0, 0, right, bottom))
-        # invert image
-        img_gray = cropped_img.convert("L")
-        img_array = np.array(img_gray)
-        max_value = np.iinfo(img_array.dtype).max
-        inverted_array = max_value - img_array
-        inverted_img = Image.fromarray(inverted_array)
-        # resize image
-        resized_image = inverted_img.resize((target_size[0], target_size[1]), resample=Image.Resampling.LANCZOS)
-        resized_image.save(target_path)
-else:
-    for index, row in tqdm(df1_sample.iterrows()):
-        image_path = row['path']
-        image_filename = os.path.basename(image_path)
-        target_path = os.path.join(sampling_path_images, image_filename)
-        shutil.copy(image_path, target_path)
-
-# uvp6
-for index, row in tqdm(df2_sample.iterrows()):
-    image_path = row['path']
-    image_filename = os.path.basename(image_path)
-    target_path = os.path.join(sampling_path_images, image_filename)
-    img = Image.open(image_path)
-    # invert image
-    img_gray = img.convert("L")
-    img_array = np.array(img_gray)
-    max_value = np.iinfo(img_array.dtype).max
-    inverted_array = max_value - img_array
-    inverted_img = Image.fromarray(inverted_array)
-    # resize image
-    resized_image = inverted_img.resize((target_size[0], target_size[1]), resample=Image.Resampling.LANCZOS)
-    resized_image.save(target_path)
+copy_image_from_df(df1_sample, sampling_path_images, target_size, cutting_ruler=True, invert_img=True)
+copy_image_from_df(df2_sample, sampling_path_images, target_size, cutting_ruler=False, invert_img=False)
 
 
 # merge two dataframe
@@ -241,22 +84,11 @@ df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 df = df.reset_index()
 df = df.replace('NaN', 0)
 
-csv_path = sampling_path / ("sampled.csv")
-df.to_csv(csv_path)
-
-df = df.rename(columns={'lat': 'latitude [deg]', })
-df = df.rename(columns={'lon': 'longitude [deg]', })
-df = df.rename(columns={'depth': 'depth [m]', })
-df['altitude [m]'] = 0
-df['roll [deg]'] = 0
-df['pitch [deg]'] = 0
-df['northing [m]'] = 0
-df['easting [m]'] = 0
-df['heading [deg]'] = 0
-df['timestamp [s]'] = 0
+selected_columns = df[['index', 'profile_id', 'object_id', 'depth', 'lat', 'lon', 'datetime',
+                       'uvp_model', 'groundtruth', 'relative_path']]
 
 csv_path = sampling_path / ("sampled_images.csv")
-df.to_csv(csv_path)
+selected_columns.to_csv(csv_path)
 
 # report
 res1 = df1.groupby(['groundtruth']).size()
