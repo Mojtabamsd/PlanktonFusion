bf3d5e061df2bb5658e075c855b385977139e54b
mojmas <masoudi.m1991@gmail.com>
2023-08-17 15:37:51 +0100

    console

--- train/train.py
+++ train/train.py
@@ -14,7 +14,9 @@ from sklearn.metrics import accuracy_score
 
 def train_cnn(config_path, input_path, output_path):
 
-    Console.info("Sampling started at", datetime.datetime.now())
+    console = Console()
+    console.info("Training started ...")
+
     config = Configuration(config_path, input_path, output_path)
 
     # Create output directory
@@ -25,8 +27,8 @@ def train_cnn(config_path, input_path, output_path):
     input_csv = input_folder / sampled_images_csv_filename
 
     if not input_csv.is_file():
-        Console.error("The input csv file", input_csv, "does not exist.")
-        Console.quit("Input csv file does not exist.")
+        console.error("The input csv file", input_csv, "does not exist.")
+        console.quit("Input csv file does not exist.")
 
     time_str = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     rel_training_path = Path("training" + time_str)
@@ -34,8 +36,8 @@ def train_cnn(config_path, input_path, output_path):
     if not training_path.exists():
         training_path.mkdir(exist_ok=True, parents=True)
     elif training_path.exists():
-        Console.error("The output folder", training_path, "exists.")
-        Console.quit("Folder exists, not overwriting previous results.")
+        console.error("The output folder", training_path, "exists.")
+        console.quit("Folder exists, not overwriting previous results.")
 
     # Save configuration file
     output_config_filename = training_path / "config.yaml"
@@ -43,23 +45,36 @@ def train_cnn(config_path, input_path, output_path):
 
     # Define data transformations
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # Resize to desired input size
+        transforms.Resize((227, 227)),  # Resize to desired input size
         transforms.ToTensor(),
     ])
 
     # Create uvp dataset datasets for training and validation
-    train_dataset = UvpDataset(csv_file=input_csv, root_dir=input_folder, transform=transform, train=True)
-    val_dataset = UvpDataset(csv_file=input_csv, root_dir=input_folder, transform=transform, train=False)
+    train_dataset = UvpDataset(csv_file=input_csv,
+                               root_dir=input_folder,
+                               transform=transform,
+                               train=True)
+
+    val_dataset = UvpDataset(csv_file=input_csv,
+                             root_dir=input_folder,
+                             transform=transform,
+                             train=False)
 
     # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=config.training.batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=config.training.batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset,
+                              batch_size=config.training.batch_size,
+                              shuffle=True)
+    val_loader = DataLoader(val_dataset,
+                            batch_size=config.training.batch_size,
+                            shuffle=False)
 
     config.num_class = train_dataset.data_frame['label'].nunique()
     device = torch.device(f'cuda:{config.base.gpu_index}' if
                           torch.cuda.is_available() and config.base.cpu is False else 'cpu')
 
-    model = SimpleCNN(config.num_class, gray=config.training.gray)
+    model = SimpleCNN(num_classes=config.num_class,
+                      gray=config.training.gray,
+                      input_size=config.sampling.target_size)
     model.to(device)
 
     # Loss criterion and optimizer
@@ -67,7 +82,7 @@ def train_cnn(config_path, input_path, output_path):
     optimizer = optim.Adam(model.parameters(), lr=config.training.learning_rate)
 
     # Training loop
-    for epoch in range(config.training.num_epochs):
+    for epoch in range(config.training.num_epoch):
         model.train()
         running_loss = 0.0
 
@@ -83,7 +98,15 @@ def train_cnn(config_path, input_path, output_path):
             running_loss += loss.item()
 
         average_loss = running_loss / len(train_loader)
-        print(f"Epoch [{epoch + 1}/{config.training.num_epoch}] - Loss: {average_loss:.4f}")
+        console.info(f"Epoch [{epoch + 1}/{config.training.num_epoch}] - Loss: {average_loss:.4f}")
+
+    # Save the model's state dictionary to a file
+    saved_weights = "model_weights.pth"
+    saved_weights_file = training_path / saved_weights
+
+    torch.save(model.state_dict(), saved_weights_file)
+
+    console.info(f"Model weights saved to {saved_weights_file}")
 
     # Evaluation loop
     model.eval()
@@ -101,7 +124,9 @@ def train_cnn(config_path, input_path, output_path):
             all_preds.extend(preds.cpu().numpy())
 
     accuracy = accuracy_score(all_labels, all_preds)
-    print(f"Validation Accuracy: {accuracy:.4f}")
+    console.info(f"Validation Accuracy: {accuracy:.4f}")
+
+    console.save_log(training_path)
 
 
 
