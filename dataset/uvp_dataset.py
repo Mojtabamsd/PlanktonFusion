5163aa52db28791792a0c67d0dd6e635ad90d2e7
mojmas <masoudi.m1991@gmail.com>
2023-08-18 14:27:18 +0100

    handle not having csv and add filename path

--- dataset/uvp_dataset.py
+++ dataset/uvp_dataset.py
@@ -8,32 +8,56 @@ from sklearn.model_selection import train_test_split
 
 
 class UvpDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, train=True):
-        self.data_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+    def __init__(self, root_dir, num_class, csv_file=None, transform=None, train=True):
+        self.root_dir = os.path.join(root_dir, 'output')
+        self.num_class = num_class
+        self.csv_file = csv_file
         self.transform = transform
         self.train = train
+
+        if self.csv_file:
+            self.data_frame = pd.read_csv(csv_file)
 
         if self.train:
             # 20% data is used for evaluation
             self.data_frame, _ = train_test_split(self.data_frame, test_size=0.2, random_state=42)
 
-        # Create a dictionary to map string labels to integer labels
-        self.label_to_int = {label: i for i, label in enumerate(self.data_frame['label'].unique())}
+        # regrouping
+        ren = pd.read_csv("./data_preparation/regrouping.csv")
+        if self.num_class == 13:
+            self.label_to_int = {label: i for i, label in enumerate(ren['regrouped2'].unique())}
+        elif self.num_class == 25:
+            self.label_to_int = {label: i for i, label in enumerate(ren['regrouped1'].unique())}
 
     def __len__(self):
-        return len(self.data_frame)
+        if self.csv_file:
+            return len(self.data_frame)
+        else:
+            return len(os.listdir(self.root_dir))
 
     def __getitem__(self, idx):
-        relative_path = self.data_frame.iloc[idx, self.data_frame.columns.get_loc('relative_path')]
-        img_name = os.path.join(self.root_dir, relative_path)
-        image = Image.open(img_name)
-        label = self.data_frame.iloc[idx, self.data_frame.columns.get_loc('label')]
+        if self.csv_file:
+            relative_path = self.data_frame.iloc[idx, self.data_frame.columns.get_loc('relative_path')]
+            img_name = os.path.basename(relative_path)
+            img_path = os.path.join(self.root_dir, img_name)
+            label = self.data_frame.iloc[idx, self.data_frame.columns.get_loc('label')]
+            int_label = self.label_to_int[label]
+            image = self.load_image(img_path)
+            return image, int_label, img_name
+        else:
+            img_name = os.path.basename(os.listdir(self.root_dir)[idx])
+            img_path = os.path.join(self.root_dir, img_name)
+            image = self.load_image(img_path)
+            return image, '', img_name
 
+    def load_image(self, img_path):
+        image = Image.open(img_path)
         if self.transform:
             image = self.transform(image)
+        return image
 
-        # Convert string label to integer label using the label_to_int dictionary
-        int_label = self.label_to_int[label]
-
-        return image, int_label
+    def get_string_label(self, int_label):
+        if self.label_to_int is not None:
+            return list(self.label_to_int.keys())[list(self.label_to_int.values()).index(int_label)]
+        else:
+            return None
