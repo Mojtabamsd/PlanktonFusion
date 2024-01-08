f8d2dcff97e8e56162756d87ebd891fc9c5f478d
mojmas <masoudi.m1991@gmail.com>
2024-01-08 11:44:32 +0000

    remove contours if bigger than avg_mean_thresh

--- models/loss.py
+++ models/loss.py
@@ -35,7 +35,8 @@ class FocalLoss(nn.Module):
 
         # Calculate focal weights
         one_hot = F.one_hot(targets, num_classes=inputs.size(1)).float()
-        focal_weights = torch.where(one_hot == 1, self.alpha * (1 - inputs.softmax(dim=1)) ** self.gamma, (1 - self.alpha) * inputs.softmax(dim=1) ** self.gamma)
+        focal_weights = torch.where(one_hot == 1, self.alpha * (1 - inputs.softmax(dim=1)) ** self.gamma,
+                                    (1 - self.alpha) * (inputs.softmax(dim=1)) ** self.gamma)
 
         # Apply the focal weights to the cross-entropy loss
         focal_loss = ce_loss * focal_weights[:, 1]
