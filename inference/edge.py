a22e50c197c4ba4c417f2538fc4f62825c7a07e7
mojmas <masoudi.m1991@gmail.com>
2024-03-26 13:27:11 +0000

    edge processing, summary report json file

--- inference/edge.py
+++ inference/edge.py
@@ -8,9 +8,8 @@ from dataset.uvp_dataset import UvpDataset
 from models.classifier_cnn import SimpleCNN, ResNetCustom, MobileNetCustom, ShuffleNetCustom, count_parameters
 import torch
 import pandas as pd
-from tools.utils import report_to_df, memory_usage
-import os
-import shutil
+from tools.utils import report_to_df, memory_usage, processing_time
+import os, json, shutil
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 
@@ -24,7 +23,7 @@ def edge(config_path, input_path, output_path):
     output_folder = Path(output_path)
 
     console = Console(output_folder)
-    console.info("Prediction started ...")
+    console.info("Model loading started ...")
 
     sampled_images_csv_filename = "sampled_images.csv"
     input_csv = input_folder / sampled_images_csv_filename
@@ -36,6 +35,7 @@ def edge(config_path, input_path, output_path):
     time_str = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     rel_prediction_path = Path("prediction" + time_str)
     prediction_path = output_folder / rel_prediction_path
+    config.prediction_path = prediction_path
     if not prediction_path.exists():
         prediction_path.mkdir(exist_ok=True, parents=True)
     elif prediction_path.exists():
@@ -85,8 +85,9 @@ def edge(config_path, input_path, output_path):
 
     model.to(device)
 
-    # test memory usage
+    # test memory and time usage
     console.info(memory_usage(config, model, device))
+    console.info(processing_time(config, model, device))
 
     # Save the model's state dictionary to a file
     saved_weights = "model_weights_final.pth"
@@ -108,14 +109,17 @@ def edge(config_path, input_path, output_path):
 
     dataloader = DataLoader(test_dataset, batch_size=config.prediction.batch_size, shuffle=False)
 
-    predict(model, dataloader, prediction_path, device)
+    predict_edge(model, dataloader, device, console, config)
 
 
-def predict(model, dataloader, prediction_path, device):
+def predict_edge(model, dataloader, device, console, config):
+    console.info("Edge processing started ...")
     model.eval()
 
     all_labels = []
     all_preds = []
+    classification_summary = []
+    total_images_processed = 0
 
     with torch.no_grad():
         for index, (images, labels, img_names) in enumerate(dataloader):
@@ -131,14 +135,26 @@ def predict(model, dataloader, prediction_path, device):
                 int_label = predicted_labels[i].item()
                 string_label = dataloader.dataset.get_string_label(int_label)
                 image_name = img_names[i]
-                image_path = os.path.join(prediction_path, string_label, image_name.replace('output/', ''))
-
-                if not os.path.exists(os.path.dirname(image_path)):
-                    os.makedirs(os.path.dirname(image_path))
-
+                image_path = os.path.join(config.prediction_path, string_label, image_name.replace('output/', ''))
                 input_path = os.path.join(dataloader.dataset.root_dir, image_name)
-                shutil.copy(input_path, image_path)
 
+                if config.edge.save_image:
+                    if not os.path.exists(os.path.dirname(image_path)):
+                        os.makedirs(os.path.dirname(image_path))
+                    shutil.copy(input_path, image_path)
+
+                log = {"time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                       "image_name": image_name,
+                       "predicted_class": string_label,
+                       "image_path": input_path}
+                classification_summary.append(log)
+
+                total_images_processed += 1
+
+                if total_images_processed % 1000 == 0:
+                    console.info(f"Total number of images processed: {total_images_processed}")
+
+        console.info(f"Total number of images processed: {total_images_processed}")
         if dataloader.dataset.csv_file:
             all_labels = np.concatenate(all_labels).ravel()
             all_preds = np.concatenate(all_preds).ravel()
@@ -156,14 +172,15 @@ def predict(model, dataloader, prediction_path, device):
             )
 
             df = report_to_df(report)
-            report_filename = os.path.join(prediction_path, 'report.csv')
+            report_filename = os.path.join(config.prediction_path, 'report.csv')
             df.to_csv(report_filename)
 
             df = pd.DataFrame(conf_mtx)
-            conf_mtx_filename = os.path.join(prediction_path, 'conf_matrix.csv')
+            conf_mtx_filename = os.path.join(config.prediction_path, 'conf_matrix.csv')
             df.to_csv(conf_mtx_filename)
 
-    model.train()
-
-
+    # summary reporting file
+    summary_filename = config.prediction_path / "summary.json"
+    with open(summary_filename, "w") as f:
+        json.dump(classification_summary, f, indent=4)
 
