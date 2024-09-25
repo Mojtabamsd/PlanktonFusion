03e5f809fe1a58879c8ead6252fe16a786a59981
mojmas <masoudi.m1991@gmail.com>
2024-09-25 09:59:10 +0100

    do not contribute the soft values in last layer

--- models/procom.py
+++ models/procom.py
@@ -145,7 +145,6 @@ class EstimatorCV():
         C = self.class_num  # Number of classes
         M = self.max_modes  # Number of modes per class
 
-        # Initialize the tensor for mode probabilities
         mode_probs = torch.zeros(N, M, device=device)
 
         # Iterate over each feature and compute similarity to each mode of its class
@@ -323,8 +322,8 @@ class ProCoMLoss(nn.Module):
         contrast_logits = contrast_logits.view(batch_size, self.num_classes, self.max_modes)
 
         # Weight the logits by the soft assignment probabilities
-        class_logits = torch.sum(contrast_logits * soft_assignments.unsqueeze(1), dim=2)
-        # class_logits = torch.max(contrast_logits, dim=2)[0]  # Max Pooling Across modes (Best Fit Mode)
+        # class_logits = torch.sum(contrast_logits * soft_assignments.unsqueeze(1), dim=2)
+        class_logits = torch.max(contrast_logits, dim=2)[0]  # Max Pooling Across modes (Best Fit Mode)
         # class_logits = torch.mean(contrast_logits, dim=2)  # Mean pooling across modes
         # class_logits = torch.logsumexp(contrast_logits, dim=2)  # Log-Sum-Exp pooling across modes
 
